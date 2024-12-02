1e8993ca4ef43c2fac5bf7680f166a50eb9aca84
eric <eric.decossaux@gmail.com>
2024-12-02 21:44:59 +0100

    adapt properties

--- exercise/python/day02/tests/test_fizz_buzz_properties.py
+++ exercise/python/day02/tests/test_fizz_buzz_properties.py
@@ -4,9 +4,13 @@ from hypothesis import given, strategies as st
 
 from games.fizz_buzz import FizzBuzz, MIN, MAX
 
+CONVERSIONS = {3:"Fizz",
+               5:"Buzz",
+               7:"Whizz",
+               11:"Bang"}
 
 class FizzBuzzProperties(unittest.TestCase):
-    fizz_buzz_strings = ["Fizz", "Buzz", "FizzBuzz"]
+    fizz_buzz_strings = ["Fizz", "Buzz", "Whizz","Bang","FizzBuzz","FizzWhizz","FizzBang","BuzzBang","WhizzBang","BuzzWhizz"]
 
     def valid_strings_for(self, x: int) -> list:
         return self.fizz_buzz_strings + [str(x)]
@@ -14,14 +18,14 @@ class FizzBuzzProperties(unittest.TestCase):
     @given(st.integers(min_value=MIN, max_value=MAX))
     def test_parse_returns_valid_string_for_numbers_between_1_and_100(self, x):
         self.assertIn(
-            FizzBuzz.convert(x),
+            FizzBuzz(CONVERSIONS).convert(x),
             self.valid_strings_for(x)
         )
 
     @given(st.integers().filter(lambda i: i < MIN or i > MAX))
     def test_parse_fails_for_numbers_out_of_range(self, x):
         self.assertIsNone(
-            FizzBuzz.convert(x)
+            FizzBuzz(CONVERSIONS).convert(x)
         )
 
 
